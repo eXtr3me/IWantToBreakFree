b43fd70243ba8754b1a5a97924bf4b6c76ae2691
Miguel Ângelo <miguelangelo_fm@hotmail.com>
2018-11-29 23:50:43 +0000

    cmt

--- src/main/java/org/academiadecodigo/variachis/delta/back_end/persistence/model/AbstractModel.java
+++ src/main/java/org/academiadecodigo/variachis/delta/back_end/persistence/model/AbstractModel.java
@@ -1,8 +1,5 @@
 package org.academiadecodigo.variachis.delta.back_end.persistence.model;
 
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
 import javax.persistence.*;
 import java.util.Date;
 
@@ -12,15 +9,6 @@ public abstract class AbstractModel implements Model {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
-    @Version
-    private Integer version;
-
-    @CreationTimestamp
-    private Date creationTime;
-
-    @UpdateTimestamp
-    private Date updateTime;
 
     @Override
     public Integer getId() {
@@ -32,37 +20,10 @@ public abstract class AbstractModel implements Model {
         this.id = id;
     }
 
-    public Integer getVersion() {
-        return version;
-    }
-
-    public void setVersion(Integer version) {
-        this.version = version;
-    }
-
-    public Date getCreationTime() {
-        return creationTime;
-    }
-
-    public void setCreationTime(Date creationTime) {
-        this.creationTime = creationTime;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
-
     @Override
     public String toString() {
         return "AbstractModel{" +
                 "id=" + id +
-                ", version=" + version +
-                ", creationTime=" + creationTime +
-                ", updateTime=" + updateTime +
                 '}';
     }
 }
